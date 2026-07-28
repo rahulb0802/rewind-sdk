6f493e3849523dca61bd390e320796bf13fd2bd5
rahulb0802 <rahulsai.billakanti11@gmail.com>
2026-07-28 16:56:45 -0500

    Wire claude agent sdk adapter exports

--- rewind_sdk/__init__.py
+++ rewind_sdk/__init__.py
@@ -1,5 +1,13 @@
 from .session import RewindSession, session
 from .adapters.langgraph import wrap_langgraph
+from .adapters.claude_agent import (
+    BUILTIN_WRITE_TOOLS,
+    rewind_hooks,
+    rewind_mcp_server,
+    sandbox_tool,
+    sdk_messages_to_dicts,
+    track_stream,
+)
 from .verification import (
     EscalationContext,
     EscalationResolution,
@@ -19,6 +27,12 @@ __all__ = [
     "RewindSession",
     "session",
     "wrap_langgraph",
+    "BUILTIN_WRITE_TOOLS",
+    "rewind_hooks",
+    "rewind_mcp_server",
+    "sandbox_tool",
+    "sdk_messages_to_dicts",
+    "track_stream",
     "EscalationContext",
     "EscalationResolution",
     "LedgerEntry",
